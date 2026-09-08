b2c480938ac126e0f9b95185a3492698806998b7
David F. <davidrsferreira@gmail.com>
2026-09-07 22:40:49 -0300

    refactor: replace mat-card with mat-expansion-panel for filters in incomes

--- src/app/pages/incomes/incomes.ts
+++ src/app/pages/incomes/incomes.ts
@@ -15,6 +15,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-incomes',
@@ -29,6 +30,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
     MatSelectModule,
     MatInputModule,
     MatDatepickerModule,
+    MatExpansionModule,
   ],
   templateUrl: './incomes.html',
   styleUrl: './incomes.css',
